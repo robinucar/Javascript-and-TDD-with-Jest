4dc7603ca37b3f16e40dc7d4c5362656c33d063c
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-01-25 12:55:31 +0000

    mapping array

---
+++ map.js
@@ -0,0 +1,7 @@
+const generateMessages = (names) => {
+  return names.map((name) => {
+    return `Hi ${name}! 50% off our best candies for you today!`;
+  });
+};
+
+console.log(generateMessages(["Robin", "Kathy"]));
