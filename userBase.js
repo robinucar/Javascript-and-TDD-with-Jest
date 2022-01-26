0ceb0876bbf2c9757f03b91b307f982375f159a1
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-01-26 16:13:12 +0000

    test and class updated

--- userBase.js
+++ userBase.js
@@ -1,38 +1,29 @@
 const User = require("./user");
 
 class UserBase {
-    constructor() {
-        this.names = [
-            new User('Uma'),
-            new User('Josh'),
-            new User('Ollie')
-        ]
-        
-    }
-    count() {
-        
-        return this.names.length
-       
-    }
+  constructor(...names) {
+    this.names = names;
+  }
+  count() {
+    return this.names.length;
+  }
 
-    getNames() {
-        return this.names.map(user => {
-            return user.getName()
-        })
-    }
+  getNames() {
+    return this.names.map((user) => {
+      return user.getName();
+    });
+  }
 
-    getIntroductions() {
-        return this.names.map(user => {
-            return user.getIntroduction()
-        })
-        
-    }
+  getIntroductions() {
+    return this.names.map((user) => {
+      return user.getIntroduction();
+    });
+  }
 }
 
-const u = new UserBase()
-console.log(u)
-console.log(u.count())
-console.log(u.getNames())
-console.log(u.getIntroductions())
+const u = new UserBase(new User('Uma'), new User('Josh'), new User('Ollie'));
+console.log(u.count());
+console.log(u.getNames());
+console.log(u.getIntroductions());
 
 module.exports = UserBase;
--- userBase.test.js
+++ userBase.test.js
@@ -1,21 +1,20 @@
-const UserBase = require('./userBase')
+const UserBase = require("./userBase");
+const User = require("./user");
 describe("userBase class", () => {
-  it("returs the number of users", () => {
-    const users = ['Uma', 'Josh', 'Ollie'];
-    const userBase = new UserBase(users);
+  const userBase = new UserBase(
+    new User("Uma"),
+    new User("Josh"),
+    new User("Ollie")
+  );
+  it("returns the number of users", () => {
     expect(userBase.count()).toEqual(3);
   });
 
   it("returns the name of users", () => {
-    const users = ['Uma', 'Josh', 'Ollie']
-
-    const userBase = new UserBase(users);
     expect(userBase.getNames()).toEqual(["Uma", "Josh", "Ollie"]);
   });
 
   it("returns the introduction of users", () => {
-    const users = ['Uma', 'Josh', 'Ollie']
-    const userBase = new UserBase(users);
     expect(userBase.getIntroductions()).toEqual([
       "Hi, my name is Uma",
       "Hi, my name is Josh",
